233bea934f4bc27e646a316f0327fb4655cac0ce
shyam <shyam20998@gmail.com>
2022-01-12 01:29:05 +0530

    created board and square components

---
+++ src/components/Square.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Square({value}) {
+    return (
+        <div>
+            <button type="button">{value}</button>
+        </div>
+    )
+}
+
+export default Square
