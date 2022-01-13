d1ae01e7815d30c1c47bd77e8a2240575e054dce
shyam <shyam20998@gmail.com>
2022-01-13 15:54:24 +0530

    displayed game history

--- src/App.js
+++ src/App.js
@@ -1,40 +1,53 @@
 import React, { useState } from 'react'
 
 import Board from './components/Board';
+import History from './components/History';
 import { calculateWinner } from './helper';
 import './styles/root.scss'
 
 function App() {
 
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [isXNext, setIsXNext] = useState(false)
+  const [history, setHistory] = useState([{board: Array(9).fill(null), isXNext: true}])
+  const [currentMove, setCurrentMove] = useState(0)
 
-  const winner = calculateWinner(board)
+  const current = history[currentMove]
 
-  const message = winner ? `Winner is ${winner}` : `Next player is ${isXNext ? 'X' : '0'}`
+  console.log(history)
 
-    const clickHandler = (position) => {
-        if (board[position] || winner) {
-            return
-        }
+  const winner = calculateWinner(current.board)
 
-        setBoard((prev) => {
-            return board.map((el, index) => {
-                if (index === position) {
-                    return isXNext ? 'X' : '0'
-                }
-                return el
-            })
-        })
+  const message = winner ? `Winner is ${winner}` : `Next player is ${current.isXNext ? 'X' : '0'}`
 
-        setIsXNext(prev => !prev)
+  const clickHandler = (position) => {
+    if (current.board[position] || winner) {
+        return
     }
+
+    setHistory((prev) => {
+      const last = prev[prev.length - 1]
+      
+      const newBoard = last.board.map((el, index) => {
+        if (index === position) {
+          return last.isXNext ? 'X' : '0'
+        }
+        return el
+      })
+      return prev.concat( {board: newBoard, isXNext: !last.isXNext})
+    })
+
+    setCurrentMove(prev => prev + 1)
+  }
+
+  const moveTo = move => {
+    setCurrentMove(move)
+  }
 
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
       <h2>{message}</h2>
-      <Board board={board} clickHandler={clickHandler}/>
+      <Board board={current.board} clickHandler={clickHandler}/>
+      <History history={history} moveTo={moveTo} currentMove={currentMove}/>
     </div>
   );
 }
